80a64a31919c12c6a886157548953b3dac7c831b
psyhs10 <psyhs10@exmail.nottingham.ac.uk>
2023-02-11 14:53:59 +0000

    Add first functional game 'inflate' starts in menu after pressing 'a'

---
+++ include/macros.h
@@ -0,0 +1,9 @@
+#include <tonc.h>
+#ifndef __MACROS__
+#define __MACROS__
+
+#define tte_init_se_flashtext(bgnr, bgcnt)\
+	tte_init_se( bgnr, bgcnt, 0xF000,CLR_WHITE , 0, &fwf_default, NULL)
+
+
+#endif // __MACROS__
--- source/main.c
+++ source/main.c
@@ -4,15 +4,34 @@
 #include <tonc.h>
 #include "macros.h"
 
-int *inflate(u16 time);
+
+//games return 1 to fail var if failed, 0 if succeded.
+void *inflate(int time,int hits,int *fail);
+
+void GAME_RETURN(){
+    REG_DISPCNT = RESET_VRAM;
+    REG_DISPCNT= DCNT_MODE0 | DCNT_BG3;
+}
 
 void flashText(char * s);
 
 int main(){
 
-    REG_DISPCNT= DCNT_MODE0 | DCNT_BG3;
+    int*fail;
+    fail = malloc(sizeof (int));
+/*
+    //INIT BACKGROUND SPRITE
+    memcpy(pal_bg_mem, brinPal, brinPalLen);
+	// Load tiles into CBB 0
+	memcpy(&tile_mem[0][0], brinTiles, brinTilesLen);
+	// Load map into SBB 30
+	memcpy(&se_mem[30][0], brinMap, brinMapLen);
 
-    tte_init_se_flashtext(3,BG_CBB(0)|BG_SBB(31));
+*/
+    REG_DISPCNT= DCNT_MODE0 | DCNT_BG0;
+
+    //INIT MENU TEXT
+    tte_init_se_flashtext(0,BG_CBB(0)|BG_SBB(31));
     tte_write("#{P:35,55}");
     tte_write("Ziggy's Mega Micro Games!");
 
@@ -24,8 +43,14 @@ int main(){
         key_poll();
         if(key_hit(KEY_A)){
             tte_erase_screen();
-            flashText("INFLATE!!    ");
-            flashText("FIGHT!!    ");
+            inflate(300,9,fail);
+            if(*fail == 1){
+            }
+            else
+            {
+                tte_init_se_default(3,BG_CBB(1)|BG_SBB(30));
+                tte_write("Succeed");
+            }
         }
     }
     //REG_DISPCNT= RESET_VRAM;
@@ -35,11 +60,11 @@ int main(){
 
 void flashText(char * s){
     int i=0;
-    //REG_DISPCNT = DCNT_MODE0 | DCNT_BG3;
+    REG_DISPCNT = DCNT_MODE0 | DCNT_BG3;
     tte_init_se_flashtext(3,BG_CBB(0)|BG_SBB(31));
     tte_write("#{P:0,20}");
 
-    while(i<120){
+    while(i<30){
         vid_vsync();
         if(i%5==0)
         {
@@ -54,12 +79,48 @@ void flashText(char * s){
 
 }
 
-int* inflate(u16 time)
+void *inflate(int time,int hits, int *fail)
 {
-
+    int h=0,t=0,p=0,i=0;
+    *fail = 1;
     REG_DISPCNT = RESET_VRAM;
-    REG_DISPCNT = DCNT_MODE0 | DCNT_BG3;
 
-    tte_init_se_flashtext(3,BG_CBB(0)|BG_SBB(31));
+    flashText("INFLATE!!!    ");
+
+    REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
+
+    tte_init_se_default(0,BG_CBB(1)|BG_SBB(30));
+   
+
+    while((h<hits) && (t<=time)){
+        vid_vsync();
+        key_poll();
+        if(key_hit(KEY_A)){
+            h++;
+            p = (h/(hits/3));
+            switch(p){
+            case 1:
+                tte_write("#{P:0,50}ONE");
+                break;
+            case 2:
+                tte_write("#{P:50,50}TWO");
+                break;
+            case 3:
+                tte_write("#{P:100,50}THREEEEE!!");
+                *fail = 0;
+                break;
+            default:
+                tte_write("#{P:10,10}HIT");
+                //tte_erase_line();
+        }
+        }
+        t++;
+    }
+
+    while(i<30){
+        i++;
+    }
+    tte_erase_screen();
+    GAME_RETURN();
     return 0;
 }
